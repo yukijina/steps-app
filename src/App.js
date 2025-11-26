ac127ba3da1dc6eb532a30e458e7e301d5bfd8c9
yuki <38411009+yukijina@users.noreply.github.com>
2025-11-26 06:38:07 -0800

    added basic css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
+import './index.css';
+
 function App() {
   return (
-    <div className='App'>
-      <div className='steps'>
-        <div className='numbers'>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-        </div>
+    <div className='steps'>
+      <div className='numbers'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
 
-        <div className='buttons'>
-          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
-            Previouw
-          </button>
-          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
-            Next
-          </button>
-        </div>
+      <p className='message'>Hello</p>
+
+      <div className='buttons'>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Previouw
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Next
+        </button>
       </div>
     </div>
   );
