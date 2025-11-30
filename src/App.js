e94bdf579bf1c2590250c772abef0055895907d6
yuki <38411009+yukijina@users.noreply.github.com>
2025-11-30 10:00:39 -0800

    impored useState and replace variable to the state. Created onClick function to buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './index.css';
+import { useState } from 'react';
 
 const messages = [
   'Learn React 💻',
@@ -7,14 +8,22 @@ const messages = [
 ];
 
 function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
 
   return (
     <div className='steps'>
       <div className='numbers'>
-        <div className={step >= 1 && 'active'}>1</div>
-        <div className={step >= 2 && 'active'}>2</div>
-        <div className={step >= 3 && 'active'}>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
 
       <p className='message'>
@@ -22,10 +31,16 @@ function App() {
       </p>
 
       <div className='buttons'>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
