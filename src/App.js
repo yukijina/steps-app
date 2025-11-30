adba175ee804cadf043fd5397c94006606461f20
yuki <38411009+yukijina@users.noreply.github.com>
2025-11-30 09:34:15 -0800

    added step

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './index.css';
 
+const messages = [
+  'Learn React 💻',
+  'Apply for jobs 📥',
+  'Invest you rincome 💰',
+];
+
 function App() {
+  const step = 1;
+
   return (
     <div className='steps'>
       <div className='numbers'>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={step >= 1 && 'active'}>1</div>
+        <div className={step >= 2 && 'active'}>2</div>
+        <div className={step >= 3 && 'active'}>3</div>
       </div>
 
-      <p className='message'>Hello</p>
+      <p className='message'>
+        {step}: {messages[step - 1]}
+      </p>
 
       <div className='buttons'>
         <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
-          Previouw
+          Previous
         </button>
         <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
           Next
