56ef5d0d2ddae842b2e8aff8dbeb742b09277d66
yuki <38411009+yukijina@users.noreply.github.com>
2025-12-02 06:23:32 -0800

    updated state with current value to callback - fixed to the safer side

--- src/App.js
+++ src/App.js
@@ -12,16 +12,16 @@ function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((step) => step - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((step) => step + 1);
   }
 
   return (
     <main>
-      <button className='close' onClick={() => setIsOpen(!isOpen)}>
+      <button className='close' onClick={() => setIsOpen((isOpen) => !isOpen)}>
         &times;
       </button>
 
